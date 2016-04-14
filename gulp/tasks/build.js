6286c65a82f68f26d725bcda5bb98d04a23b2686
Einari Naukkarinen <einari.naukkarinen@basware.com>
2016-04-14 14:49:08 +0300

    reworked dev task

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -2,8 +2,8 @@
 var gulp   = require('gulp');
 var config = require('../config');
 
+gulp.task('dev', ['watch', 'styles', 'vendor']);
 gulp.task('build', ['build-ts', 'styles', 'vendor']);
-
 gulp.task('watch', ['watch-ts', 'watch-html', 'watch-less']);
 
 gulp.task('watch-html', function () {
