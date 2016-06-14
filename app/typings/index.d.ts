c5917182d8a212dad0052616c5456a29816cad46
Einari Naukkarinen <einari.naukkarinen@tieto.com>
2016-06-14 17:24:13 +0300

    added jquery types

--- app/typings/index.d.ts
+++ app/typings/index.d.ts
@@ -1,3 +1,4 @@
 /// <reference path="globals/angular/index.d.ts" />
+/// <reference path="globals/jquery/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
 /// <reference path="modules/requirejs/index.d.ts" />
