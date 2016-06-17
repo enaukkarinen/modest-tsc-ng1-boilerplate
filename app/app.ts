f1d3b74916a146d5aadddf2d79ae250fa2b231aa
Einari Naukkarinen <einari.naukkarinen@tieto.com>
2016-06-17 14:16:12 +0300

    build fix

--- app/app.ts
+++ app/app.ts
@@ -20,7 +20,7 @@ export function start() {
         }]);
 
         // manually bootstrap angular
-        angular.bootstrap(document, ['app']);
+        angular.bootstrap(document, ['app'], {strictDi: true});
     });
 }
 
